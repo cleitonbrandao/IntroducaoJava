370abc76b42417e3f087d1f26b39c4ef62504fff
Cleiton <cbs3dmax@gmail.com>
2021-03-16 02:36:10 -0300

    Problemas resolvidos site URI

---
+++ inicioJava/src/inicioJava/tetesJavaUri.java
@@ -0,0 +1,49 @@
+package inicioJava;
+import java.util.Scanner;
+public class tetesJavaUri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Scanner ler = new Scanner(System.in);
+        double raio = ler.nextDouble();
+        double volume = (4/3.0) * 3.14159 * Math.pow(raio, 3); 
+        
+ ---------------------------------------------------------------------       
+        
+        System.out.printf("VOLUME = %.3f%n", volume);
+		
+		Scanner ler = new Scanner(System.in);
+        double a = ler.nextDouble();
+        double b = ler.nextDouble();
+        double c = ler.nextDouble();
+        
+        double ra, rb, rc, rd, re;
+        
+        ra = (a * c) / 2.0;
+        rb = Math.pow(c, 2.0) * 3.14159;
+        rc = (c * (a + b)) / 2.0;
+        rd = b * b;
+        re = a * b;
+        
+        System.out.printf("TRIANGULO: %.3f%nCIRCULO: %.3f%nTRAPEZIO: %.3f%nQUADRADO: %.3f%nRETANGULO: %.3f%n", ra, rb, rc, rd, re);
+        
+        ler.close();
+-----------------------------------------------------------------------------------------------------------
+		
+		
+		ESTRUTURA SE / IF - ELSE IF - ELSE
+Scanner ler = new Scanner(System.in);
+int a = ler.nextInt();
+int b = ler.nextInt();
+int c = ler.nextInt();
+
+if ( a > b && a > c) {
+    System.out.println(a + " eh o maior");
+}else if( b > c) {
+    System.out.println(b + " eh o maior");
+ }else {
+    System.out.println(c + " eh o maior");
+} */
+	}
+
+}
